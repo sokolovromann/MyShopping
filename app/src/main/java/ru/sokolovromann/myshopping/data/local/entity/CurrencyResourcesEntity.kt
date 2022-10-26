83a3de8b7449526bb6186853f39f04342abb71d6
Roman Sokolov <sokolovromann@gmail.com>
2022-10-26 17:12:35 +0300

    Add local currency resources entity

---
+++ app/src/main/java/ru/sokolovromann/myshopping/data/local/entity/CurrencyResourcesEntity.kt
@@ -0,0 +1,6 @@
+package ru.sokolovromann.myshopping.data.local.entity
+
+data class CurrencyResourcesEntity(
+    val defaultCurrency: String = "",
+    val defaultCurrencyDisplayToLeft: Boolean = false
+)
--- app/src/main/java/ru/sokolovromann/myshopping/data/local/resources/MainResources.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/local/resources/MainResources.kt
@@ -2,17 +2,17 @@ package ru.sokolovromann.myshopping.data.local.resources
 
 import android.content.res.Resources
 import ru.sokolovromann.myshopping.R
+import ru.sokolovromann.myshopping.data.local.entity.CurrencyResourcesEntity
 import javax.inject.Inject
 
 class MainResources @Inject constructor(
     private val resources: Resources
 ) {
 
-    fun getDefaultCurrency(): String {
-        return resources.getString(R.string.data_defaultCurrency)
-    }
-
-    fun getDefaultCurrencyDisplayToLeft(): Boolean {
-        return resources.getBoolean(R.bool.data_defaultCurrencyDisplayToLeft)
+    fun getCurrencyResources(): CurrencyResourcesEntity {
+        return CurrencyResourcesEntity(
+            defaultCurrency = resources.getString(R.string.data_defaultCurrency),
+            defaultCurrencyDisplayToLeft = resources.getBoolean(R.bool.data_defaultCurrencyDisplayToLeft)
+        )
     }
 }
