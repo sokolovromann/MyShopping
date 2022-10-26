af8a677eee83c581239f0c37d50fb3b839a8d185
Roman Sokolov <sokolovromann@gmail.com>
2022-10-26 17:12:04 +0300

    Add local settings resources entity

---
+++ app/src/main/java/ru/sokolovromann/myshopping/data/local/entity/SettingsResourcesEntity.kt
@@ -0,0 +1,7 @@
+package ru.sokolovromann.myshopping.data.local.entity
+
+data class SettingsResourcesEntity(
+    val developerName: String = "",
+    val developerEmail: String = "",
+    val appGithubLink: String = ""
+)
--- app/src/main/java/ru/sokolovromann/myshopping/data/local/resources/SettingsResources.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/local/resources/SettingsResources.kt
@@ -2,21 +2,18 @@ package ru.sokolovromann.myshopping.data.local.resources
 
 import android.content.res.Resources
 import ru.sokolovromann.myshopping.R
+import ru.sokolovromann.myshopping.data.local.entity.SettingsResourcesEntity
 import javax.inject.Inject
 
 class SettingsResources @Inject constructor(
     private val resources: Resources
 ) {
 
-    fun getDeveloperName(): String {
-        return resources.getString(R.string.data_developerName)
-    }
-
-    fun getDeveloperEmail(): String {
-        return resources.getString(R.string.data_developerEmail)
-    }
-
-    fun getGithubLink(): String {
-        return resources.getString(R.string.data_githubLink)
+    fun getSettingsResources(): SettingsResourcesEntity {
+        return SettingsResourcesEntity(
+            developerName = resources.getString(R.string.data_developerName),
+            developerEmail = resources.getString(R.string.data_developerEmail),
+            appGithubLink = resources.getString(R.string.data_githubLink)
+        )
     }
 }
