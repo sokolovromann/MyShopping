c33d6aeb20c4fb3b48283bfa46059fc4747ba4d8
Roman Sokolov <sokolovromann@gmail.com>
2022-12-03 18:42:28 +0300

    Add discount as percent menu selected

--- app/src/main/java/ru/sokolovromann/myshopping/ui/compose/state/DiscountAsPercentMenu.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/compose/state/DiscountAsPercentMenu.kt
@@ -2,5 +2,7 @@ package ru.sokolovromann.myshopping.ui.compose.state
 
 data class DiscountAsPercentMenu(
     val asPercentBody: TextData = TextData(),
-    val asMoneyBody: TextData = TextData()
+    val asPercentSelected: RadioButtonData = RadioButtonData(),
+    val asMoneyBody: TextData = TextData(),
+    val asMoneySelected: RadioButtonData = RadioButtonData(),
 )
