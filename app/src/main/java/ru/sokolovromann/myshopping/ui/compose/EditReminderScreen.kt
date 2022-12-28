3320a00894ab98f4d88464b01788faed59c05c4f
Roman Sokolov <sokolovromann@gmail.com>
2022-12-28 17:00:07 +0300

    Refactoring EditReminderViewModel

--- app/src/main/java/ru/sokolovromann/myshopping/ui/compose/EditReminderScreen.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/compose/EditReminderScreen.kt
@@ -1,9 +1,5 @@
 package ru.sokolovromann.myshopping.ui.compose
 
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
-import android.widget.DatePicker
-import android.widget.TimePicker
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -11,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -26,6 +21,7 @@ fun EditReminderScreen(
     navController: NavController,
     viewModel: EditReminderViewModel = hiltViewModel()
 ) {
+    val screenData = viewModel.editReminderState.screenData
 
     LaunchedEffect(Unit) {
         viewModel.screenEventFlow.collect {
@@ -37,125 +33,103 @@ fun EditReminderScreen(
 
     AppDialog(
         onDismissRequest = { viewModel.onEvent(EditReminderEvent.CancelSavingReminder) },
-        header = { Text(text = viewModel.editReminderState.screenData.headerText.asCompose()) },
-        actionButtons = { ActionButtons(viewModel) },
-        content = { Content(viewModel) }
-    )
-}
-
-@Composable
-private fun ActionButtons(viewModel: EditReminderViewModel) {
-    Column(
-        horizontalAlignment = Alignment.End,
-        verticalArrangement = Arrangement.Center
-    ) {
-        val screenData = viewModel.editReminderState.screenData
-        if (screenData.showDeleteButton) {
-            AppDialogActionButton(
-                onClick = { viewModel.onEvent(EditReminderEvent.DeleteReminder) },
-                content = {
-                    Text(text = stringResource(R.string.editReminder_action_deleteReminder))
+        header = { Text(text = screenData.headerText.asCompose()) },
+        actionButtons = {
+            Column(
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Center
+            ) {
+                if (screenData.showDeleteButton) {
+                    AppDialogActionButton(
+                        onClick = { viewModel.onEvent(EditReminderEvent.DeleteReminder) },
+                        content = {
+                            Text(text = stringResource(R.string.editReminder_action_deleteReminder))
+                        }
+                    )
                 }
-            )
-        }
+                Row {
+                    AppDialogActionButton(
+                        onClick = { viewModel.onEvent(EditReminderEvent.CancelSavingReminder) },
+                        content = {
+                            Text(text = stringResource(R.string.editReminder_action_cancelSavingReminder))
+                        }
+                    )
+                    AppDialogActionButton(
+                        onClick = { viewModel.onEvent(EditReminderEvent.SaveReminder) },
+                        primaryButton = true,
+                        content = {
+                            Text(text = stringResource(R.string.editReminder_action_saveReminder))
+                        }
+                    )
+                }
+            }
+        },
+    ) {
         Row {
-            AppDialogActionButton(
-                onClick = { viewModel.onEvent(EditReminderEvent.CancelSavingReminder) },
-                content = { Text(text = viewModel.cancelState.value.text.asCompose()) }
+            OutlinedButton(
+                modifier = Modifier.weight(0.5f),
+                onClick = { viewModel.onEvent(EditReminderEvent.SelectReminderDate) },
+                contentPadding = EditReminderContentPadding
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start,
+                    content = { Text(text = screenData.dateText.asCompose()) }
+                )
+            }
+
+            Spacer(modifier = Modifier.size(EditReminderSpacerMediumSize))
+
+            OutlinedButton(
+                modifier = Modifier.weight(0.5f),
+                onClick = { viewModel.onEvent(EditReminderEvent.SelectReminderTime) },
+                contentPadding = EditReminderContentPadding
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start,
+                    content = { Text(text = screenData.timeText.asCompose()) }
+                )
+            }
+        }
+
+        if (screenData.showDateDialog) {
+            AppDatePickerDialog(
+                onDismissRequest = {
+                    viewModel.onEvent(EditReminderEvent.CancelSelectingReminderDate)
+                },
+                onDateChanged = { year: Int, month: Int, dayOfMonth: Int ->
+                    val event = EditReminderEvent.ReminderDateChanged(year, month, dayOfMonth)
+                    viewModel.onEvent(event)
+                },
+                dialogStyle = screenData.dateTimeDialogStyle(),
+                year = screenData.dateYear,
+                monthOfYear = screenData.dateMonth,
+                dayOfMonth = screenData.dateDayOfMonth
             )
-            AppDialogActionButton(
-                onClick = { viewModel.onEvent(EditReminderEvent.SaveReminder) },
-                primaryButton = true,
-                content = { Text(text = viewModel.saveState.value.text.asCompose()) }
+        }
+
+        if (screenData.showTimeDialog) {
+            AppTimePickerDialog(
+                onDismissRequest = {
+                    viewModel.onEvent(EditReminderEvent.CancelSelectingReminderTime)
+                },
+                onTimeChanged = { hourOfDay: Int, minute: Int ->
+                    val event = EditReminderEvent.ReminderTimeChanged(hourOfDay, minute)
+                    viewModel.onEvent(event)
+                },
+                dialogStyle = screenData.dateTimeDialogStyle(),
+                hourOfDay = screenData.timeHourOfDay,
+                minute = screenData.timeMinute,
+                is24HourFormat = screenData.isTime24HourFormat()
             )
         }
     }
 }
 
-@Composable
-private fun Content(viewModel: EditReminderViewModel) {
-    val screenData = viewModel.editReminderState.screenData
-
-    Row {
-        OutlinedButton(
-            modifier = Modifier.weight(0.5f),
-            onClick = { viewModel.onEvent(EditReminderEvent.SelectReminderDate) },
-            contentPadding = PaddingValues(horizontal = 8.dp)
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.Start,
-                content = { Text(text = screenData.dateText.asCompose()) }
-            )
-        }
-
-        Spacer(modifier = Modifier.size(8.dp))
-
-        OutlinedButton(
-            modifier = Modifier.weight(0.5f),
-            onClick = { viewModel.onEvent(EditReminderEvent.SelectReminderTime) },
-            contentPadding = PaddingValues(horizontal = 8.dp)
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.Start,
-                content = { Text(text = screenData.timeText.asCompose()) }
-            )
-        }
-    }
-
-    DateDialog(viewModel)
-    TimeDialog(viewModel)
-}
-
-@Composable
-private fun DateDialog(viewModel: EditReminderViewModel) {
-    val screenData = viewModel.editReminderState.screenData
-    if (!screenData.showDateDialog) { return }
-
-    val onDateSetListener = { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
-        val event = EditReminderEvent.ReminderDateChanged(year, month, dayOfMonth)
-        viewModel.onEvent(event)
-    }
-
-    val context = LocalContext.current
-
-    DatePickerDialog(
-        context,
-        screenData.dateTimeDialogStyle(),
-        onDateSetListener,
-        screenData.dateYear,
-        screenData.dateMonth,
-        screenData.dateDayOfMonth
-    ).apply {
-        setOnCancelListener { viewModel.onEvent(EditReminderEvent.CancelSelectingReminderDate) }
-        show()
-    }
-}
-
-@Composable
-private fun TimeDialog(viewModel: EditReminderViewModel) {
-    val screenData = viewModel.editReminderState.screenData
-    if (!screenData.showTimeDialog) { return }
-
-    val context = LocalContext.current
-
-    val onTimeSetListener = { _: TimePicker, hourOfDay: Int, minute: Int ->
-        val event = EditReminderEvent.ReminderTimeChanged(hourOfDay, minute)
-        viewModel.onEvent(event)
-    }
-
-    TimePickerDialog(
-        context,
-        screenData.dateTimeDialogStyle(),
-        onTimeSetListener,
-        screenData.timeHourOfDay,
-        screenData.timeMinute,
-        screenData.isTime24HourFormat()
-    ).apply {
-        setOnCancelListener { viewModel.onEvent(EditReminderEvent.CancelSelectingReminderTime) }
-        show()
-    }
-}
+private val EditReminderContentPadding = PaddingValues(
+    horizontal = 8.dp
+)
+private val EditReminderSpacerMediumSize = 8.dp
--- app/src/main/java/ru/sokolovromann/myshopping/ui/viewmodel/EditReminderViewModel.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/viewmodel/EditReminderViewModel.kt
@@ -1,8 +1,5 @@
 package ru.sokolovromann.myshopping.ui.viewmodel
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,22 +10,18 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.sokolovromann.myshopping.AppDispatchers
-import ru.sokolovromann.myshopping.R
 import ru.sokolovromann.myshopping.data.repository.EditReminderRepository
 import ru.sokolovromann.myshopping.data.repository.model.EditReminder
-import ru.sokolovromann.myshopping.data.repository.model.FontSize
 import ru.sokolovromann.myshopping.notification.purchases.PurchasesAlarmManager
 import ru.sokolovromann.myshopping.ui.*
 import ru.sokolovromann.myshopping.ui.compose.event.EditReminderScreenEvent
 import ru.sokolovromann.myshopping.ui.compose.state.EditReminderState
-import ru.sokolovromann.myshopping.ui.compose.state.TextData
 import ru.sokolovromann.myshopping.ui.viewmodel.event.EditReminderEvent
 import javax.inject.Inject
 
 @HiltViewModel
 class EditReminderViewModel @Inject constructor(
     private val repository: EditReminderRepository,
-    private val mapping: ViewModelMapping,
     private val dispatchers: AppDispatchers,
     private val savedStateHandle: SavedStateHandle,
     private val alarmManager: PurchasesAlarmManager
@@ -36,21 +29,11 @@ class EditReminderViewModel @Inject constructor(
 
     val editReminderState: EditReminderState = EditReminderState()
 
-    private val _cancelState: MutableState<TextData> = mutableStateOf(TextData())
-    val cancelState: State<TextData> = _cancelState
-
-    private val _saveState: MutableState<TextData> = mutableStateOf(TextData())
-    val saveState: State<TextData> = _saveState
-
     private val _screenEventFlow: MutableSharedFlow<EditReminderScreenEvent> = MutableSharedFlow()
     val screenEventFlow: SharedFlow<EditReminderScreenEvent> = _screenEventFlow
 
-    private val uid: String? = savedStateHandle.get<String>(UiRouteKey.ShoppingUid.key)
-
     init {
-        showCancelButton()
-        showSaveButton()
-        getReminder()
+        getEditReminder()
     }
 
     override fun onEvent(event: EditReminderEvent) {
@@ -75,13 +58,20 @@ class EditReminderViewModel @Inject constructor(
         }
     }
 
-    private fun getReminder() = viewModelScope.launch(dispatchers.io) {
+    private fun getEditReminder() = viewModelScope.launch {
+        val uid: String? = savedStateHandle.get<String>(UiRouteKey.ShoppingUid.key)
         repository.getEditReminder(uid).firstOrNull()?.let {
-            showReminder(it)
+            editReminderLoaded(it)
         }
     }
 
-    private fun saveReminder() = viewModelScope.launch(dispatchers.io) {
+    private suspend fun editReminderLoaded(
+        editReminder: EditReminder
+    ) = withContext(dispatchers.main) {
+        editReminderState.populate(editReminder)
+    }
+
+    private fun saveReminder() = viewModelScope.launch {
         val shoppingList = editReminderState.getShoppingListResult()
             .getOrElse { return@launch }
 
@@ -95,16 +85,15 @@ class EditReminderViewModel @Inject constructor(
 
         withContext(dispatchers.main) {
             alarmManager.createReminder(
-                uid = uid ?: "",
+                uid = shoppingList.uid,
                 reminder = reminder
             )
+            _screenEventFlow.emit(EditReminderScreenEvent.ShowBackScreen)
         }
-
-        showBackScreen()
     }
 
     private fun cancelSavingReminder() = viewModelScope.launch(dispatchers.main) {
-        showBackScreen()
+        _screenEventFlow.emit(EditReminderScreenEvent.ShowBackScreen)
     }
 
     private fun cancelSelectingReminderDate() {
@@ -123,7 +112,7 @@ class EditReminderViewModel @Inject constructor(
         editReminderState.selectReminderTime()
     }
 
-    private fun deleteReminder() = viewModelScope.launch(dispatchers.io) {
+    private fun deleteReminder() = viewModelScope.launch {
         val shoppingList = editReminderState.getShoppingListResult()
             .getOrElse { return@launch }
 
@@ -134,9 +123,8 @@ class EditReminderViewModel @Inject constructor(
 
         withContext(dispatchers.main) {
             alarmManager.deleteReminder(shoppingList.uid)
+            _screenEventFlow.emit(EditReminderScreenEvent.ShowBackScreen)
         }
-
-        showBackScreen()
     }
 
     private fun reminderDateChanged(event: EditReminderEvent.ReminderDateChanged) {
@@ -145,27 +133,5 @@ class EditReminderViewModel @Inject constructor(
 
     private fun reminderTimeChanged(event: EditReminderEvent.ReminderTimeChanged) {
         editReminderState.changeReminderTime(event.hourOfDay, event.minute)
-    }
-
-    private suspend fun showReminder(editReminder: EditReminder) = withContext(dispatchers.main) {
-        editReminderState.populate(editReminder)
-    }
-
-    private fun showCancelButton() {
-        _cancelState.value = mapping.toBody(
-            text = mapping.toResourcesUiText(R.string.editReminder_action_cancelSavingReminder),
-            fontSize = FontSize.MEDIUM
-        )
-    }
-
-    private fun showSaveButton() {
-        _saveState.value = mapping.toBody(
-            text = mapping.toResourcesUiText(R.string.editReminder_action_saveReminder),
-            fontSize = FontSize.MEDIUM
-        )
-    }
-
-    private fun showBackScreen() = viewModelScope.launch(dispatchers.main) {
-        _screenEventFlow.emit(EditReminderScreenEvent.ShowBackScreen)
     }
 }
