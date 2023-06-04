0a72e5bc18dd252a989f73847f82bfeef5c49ff2
Roman Sokolov <sokolovromann@gmail.com>
2023-06-04 15:30:24 +0300

    Add text warning in EditShoppingListTotal

--- app/src/main/java/ru/sokolovromann/myshopping/ui/compose/EditShoppingListTotalScreen.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/compose/EditShoppingListTotalScreen.kt
@@ -1,8 +1,11 @@
 package ru.sokolovromann.myshopping.ui.compose
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,6 +18,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -90,5 +94,16 @@ fun EditShoppingListTotalScreen(
                 onDone = { viewModel.onEvent(EditShoppingListTotalEvent.SaveShoppingListTotal) }
             )
         )
+
+        Spacer(modifier = Modifier.size(EditShoppingListTotalSpacerSmallSize))
+
+        Text(
+            text = stringResource(R.string.editShoppingListTotal_message_totalWarning),
+            style = MaterialTheme.typography.body1.copy(
+                color = MaterialTheme.colors.onSurface
+            )
+        )
     }
 }
+
+private val EditShoppingListTotalSpacerSmallSize = 8.dp
