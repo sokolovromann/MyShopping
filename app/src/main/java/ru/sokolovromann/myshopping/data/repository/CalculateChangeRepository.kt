f936b9d15ad7f10e22e7c5203c8b8c5eccd091b9
Roman Sokolov <sokolovromann@gmail.com>
2023-09-07 07:20:08 +0300

    Delete before refactoring repositories

--- app/src/main/java/ru/sokolovromann/myshopping/data/repository/CalculateChangeRepository.kt
+++
@@ -1,9 +0,0 @@
-package ru.sokolovromann.myshopping.data.repository
-
-import kotlinx.coroutines.flow.Flow
-import ru.sokolovromann.myshopping.data.repository.model.CalculateChange
-
-interface CalculateChangeRepository {
-
-    suspend fun getCalculateChange(uid: String?): Flow<CalculateChange>
-}
