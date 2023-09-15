0dabda0aacb5197f1da71425b7e273974dbaef6d
Roman Sokolov <sokolovromann@gmail.com>
2023-09-15 08:11:48 +0300

    Refactoring AddEditAutocomplete model

--- app/src/main/java/ru/sokolovromann/myshopping/data/repository/model/AddEditAutocomplete.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/repository/model/AddEditAutocomplete.kt
@@ -1,6 +1,35 @@
 package ru.sokolovromann.myshopping.data.repository.model
 
 data class AddEditAutocomplete(
-    val autocomplete: Autocomplete? = null,
-    val appConfig: AppConfig = AppConfig()
-)
+    private val autocomplete: Autocomplete? = null,
+    private val appConfig: AppConfig = AppConfig()
+) {
+
+    private val _autocomplete = autocomplete ?: Autocomplete()
+    private val userPreferences = appConfig.userPreferences
+
+    fun createAutocomplete(name: String?): Result<Autocomplete> {
+        return if (name.isNullOrEmpty()) {
+            val exception = IllegalArgumentException("Name must not be null or empty")
+            Result.failure(exception)
+        } else {
+            val success = _autocomplete.copy(
+                name = name.trim(),
+                lastModified = System.currentTimeMillis()
+            )
+            Result.success(success)
+        }
+    }
+
+    fun getFieldName(): String {
+        return _autocomplete.name
+    }
+
+    fun getFontSize(): FontSize {
+        return userPreferences.fontSize
+    }
+
+    fun isNewAutocomplete(): Boolean {
+        return autocomplete == null
+    }
+}
--- app/src/main/java/ru/sokolovromann/myshopping/ui/compose/state/AddEditAutocompleteState.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/compose/state/AddEditAutocompleteState.kt
@@ -3,41 +3,35 @@ package ru.sokolovromann.myshopping.ui.compose.state
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import ru.sokolovromann.myshopping.R
 import ru.sokolovromann.myshopping.data.repository.model.AddEditAutocomplete
 import ru.sokolovromann.myshopping.data.repository.model.Autocomplete
 import ru.sokolovromann.myshopping.data.repository.model.FontSize
-import ru.sokolovromann.myshopping.ui.utils.isEmpty
+import ru.sokolovromann.myshopping.ui.utils.toTextFieldValue
 
 class AddEditAutocompleteState {
 
-    private var autocomplete by mutableStateOf(Autocomplete())
+    private var addEditAutocomplete by mutableStateOf(AddEditAutocomplete())
 
     var screenData by mutableStateOf(AddEditAutocompleteScreenData())
         private set
 
     fun populate(addEditAutocomplete: AddEditAutocomplete) {
-        autocomplete = addEditAutocomplete.autocomplete ?: Autocomplete()
+        this.addEditAutocomplete = addEditAutocomplete
 
-        val headerText: UiText = if (addEditAutocomplete.autocomplete == null) {
+        val headerText: UiText = if (addEditAutocomplete.isNewAutocomplete()) {
             UiText.FromResources(R.string.addEditAutocomplete_header_addAutocomplete)
         } else {
             UiText.FromResources(R.string.addEditAutocomplete_header_editAutocomplete)
         }
-        val name = autocomplete.name
 
         screenData = AddEditAutocompleteScreenData(
             screenState = ScreenState.Showing,
             headerText = headerText,
-            nameValue = TextFieldValue(
-                text = name,
-                selection = TextRange(name.length),
-                composition = TextRange(name.length)
-            ),
+            nameValue = addEditAutocomplete.getFieldName().toTextFieldValue(),
             showNameError = false,
-            fontSize = addEditAutocomplete.appConfig.userPreferences.fontSize
+            fontSize = addEditAutocomplete.getFontSize()
         )
     }
 
@@ -49,16 +43,12 @@ class AddEditAutocompleteState {
     }
 
     fun getAutocompleteResult(): Result<Autocomplete> {
-        return if (screenData.nameValue.isEmpty()) {
+        val result = addEditAutocomplete.createAutocomplete(screenData.nameValue.text).getOrNull()
+        return if (result == null) {
             screenData = screenData.copy(showNameError = true)
             Result.failure(Exception())
         } else {
-            screenData = screenData.copy(screenState = ScreenState.Saving)
-            val success = autocomplete.copy(
-                name = screenData.nameValue.text.trim(),
-                lastModified = System.currentTimeMillis()
-            )
-            Result.success(success)
+            Result.success(result)
         }
     }
 }
