73749b0622ed278834653de38e7a10304bcb4d76
Roman Sokolov <sokolovromann@gmail.com>
2023-10-29 16:23:00 +0300

    Add shopping createDefaultReminder

--- app/src/main/java/ru/sokolovromann/myshopping/data/model/Shopping.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/model/Shopping.kt
@@ -1,5 +1,10 @@
 package ru.sokolovromann.myshopping.data.model
 
+import ru.sokolovromann.myshopping.data.utils.addTime
+import ru.sokolovromann.myshopping.data.utils.getHourMinute
+import ru.sokolovromann.myshopping.data.utils.toDateTime
+import java.util.Calendar
+
 data class Shopping(
     val id: Int = IdDefaults.NO_ID,
     val position: Int = IdDefaults.FIRST_POSITION,
@@ -14,4 +19,19 @@ data class Shopping(
     val sort: Sort = Sort(),
     val sortFormatted: Boolean = false,
     val pinned: Boolean = false
-)
+) {
+
+    fun createDefaultReminder(): DateTime {
+        val calendar = Calendar.getInstance().apply {
+            val hourMinute = getHourMinute()
+            val newMinute = if (hourMinute.second in 0..30) {
+                hourMinute.second - 30
+            } else {
+                60 - hourMinute.second
+            }
+            addTime(1, newMinute)
+        }
+
+        return calendar.toDateTime()
+    }
+}
--- app/src/main/java/ru/sokolovromann/myshopping/data/utils/CalendarUtils.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/utils/CalendarUtils.kt
@@ -1,5 +1,6 @@
 package ru.sokolovromann.myshopping.data.utils
 
+import ru.sokolovromann.myshopping.data.model.DateTime
 import java.util.Calendar
 
 fun Calendar.setDate(year: Int, month: Int, dayOfMonth: Int) {
@@ -11,6 +12,17 @@ fun Calendar.setDate(year: Int, month: Int, dayOfMonth: Int) {
 fun Calendar.setTime(hourOfDay: Int, minute: Int) {
     set(Calendar.HOUR_OF_DAY, hourOfDay)
     set(Calendar.MINUTE, minute)
+}
+
+fun Calendar.addDate(year: Int, month: Int, dayOfMonth: Int) {
+    add(Calendar.YEAR, year)
+    add(Calendar.MONTH, month)
+    add(Calendar.DAY_OF_MONTH, dayOfMonth)
+}
+
+fun Calendar.addTime(hourOfDay: Int, minute: Int) {
+    add(Calendar.HOUR_OF_DAY, hourOfDay)
+    add(Calendar.MINUTE, minute)
 }
 
 fun Calendar.getYearMonthDay(): Triple<Int, Int, Int> {
@@ -26,4 +38,8 @@ fun Calendar.getHourMinute(): Pair<Int, Int> {
         first = get(Calendar.HOUR_OF_DAY),
         second = get(Calendar.MINUTE)
     )
+}
+
+fun Calendar.toDateTime(): DateTime {
+    return DateTime(timeInMillis)
 }
