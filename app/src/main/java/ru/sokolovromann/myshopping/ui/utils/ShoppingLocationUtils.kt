c21ca1d4eeb3d96b31462744b20ccb89e7decbc3
Roman Sokolov <sokolovromann@gmail.com>
2023-10-29 08:25:24 +0300

    Add ShoppingLocationUtils

---
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/utils/ShoppingLocationUtils.kt
@@ -0,0 +1,11 @@
+package ru.sokolovromann.myshopping.ui.utils
+
+import ru.sokolovromann.myshopping.R
+import ru.sokolovromann.myshopping.data.model.ShoppingLocation
+import ru.sokolovromann.myshopping.ui.compose.state.UiText
+
+fun ShoppingLocation.getText(): UiText = when (this) {
+    ShoppingLocation.PURCHASES -> UiText.FromResources(R.string.shoppingLists_action_selectPurchasesLocation)
+    ShoppingLocation.ARCHIVE -> UiText.FromResources(R.string.shoppingLists_action_selectArchiveLocation)
+    ShoppingLocation.TRASH -> UiText.FromResources(R.string.shoppingLists_action_selectTrashLocation)
+}
