fe26a1589ab987f465100c953f664b7de970bab5
Roman Sokolov <sokolovromann@gmail.com>
2023-12-15 18:54:46 +0300

    Add create ScreenState function

--- app/src/main/java/ru/sokolovromann/myshopping/ui/compose/state/ScreenState.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/compose/state/ScreenState.kt
@@ -2,5 +2,15 @@ package ru.sokolovromann.myshopping.ui.compose.state
 
 @Deprecated("Will be deleted")
 enum class ScreenState {
-    Nothing, Loading, Showing, Saving
+    Nothing, Loading, Showing, Saving;
+
+    companion object {
+        fun create(waiting: Boolean, notFound: Boolean): ScreenState {
+            return if (waiting) {
+                Loading
+            } else {
+                if (notFound) Nothing else Showing
+            }
+        }
+    }
 }
