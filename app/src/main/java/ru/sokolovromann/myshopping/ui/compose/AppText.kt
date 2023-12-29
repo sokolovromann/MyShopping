8c5831ceb83ec3f361bbc407de8dc32caf4ebb07
Roman Sokolov <sokolovromann@gmail.com>
2023-12-29 14:17:29 +0300

    Delete UiText

--- app/src/main/java/ru/sokolovromann/myshopping/ui/compose/AppText.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/compose/AppText.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.sokolovromann.myshopping.data.model.FontSize
-import ru.sokolovromann.myshopping.ui.compose.state.UiText
+import ru.sokolovromann.myshopping.ui.model.UiString
 import ru.sokolovromann.myshopping.ui.utils.toItemTitle
 
 @Composable
@@ -34,7 +34,7 @@ fun AppTextGridHeader(
 @Composable
 fun AppTextGridHeader(
     modifier: Modifier = Modifier,
-    text: UiText,
+    text: UiString,
     fontSize: FontSize
 ) {
     AppTextGridHeader(
