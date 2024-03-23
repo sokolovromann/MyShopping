83ff39b177c7162aac4fd6a0fc9f678dfd09345f
Roman Sokolov <sokolovromann@gmail.com>
2024-03-23 10:39:56 +0300

    Fix bug: don't sort products after sort saved

--- app/src/main/java/ru/sokolovromann/myshopping/data/utils/ListUtils.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/utils/ListUtils.kt
@@ -51,7 +51,11 @@ fun List<ShoppingList>.sortedShoppingLists(
         displayCompleted
     }
     val sortedShoppingList = sortedShoppings.map {
-        val productsSort = Sort(sortBy = SortBy.POSITION, ascending = true)
+        val productsSort = if (it.shopping.sortFormatted) {
+            it.shopping.sort
+        } else {
+            Sort(sortBy = SortBy.POSITION, ascending = true)
+        }
         it.copy(products = it.products.sortedProducts(productsSort, productsDisplayCompleted))
     }
     val partition = sortedShoppingList.partition { it.isCompleted() }
