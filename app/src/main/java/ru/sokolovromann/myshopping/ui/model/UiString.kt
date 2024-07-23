78085fcc2d56c45833a097e62d5af05082bfcbec
Roman Sokolov <sokolovromann@gmail.com>
2024-07-23 07:12:50 +0300

    Add UiString.FromUiStrings

--- app/src/main/java/ru/sokolovromann/myshopping/ui/model/UiString.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/model/UiString.kt
@@ -8,22 +8,52 @@ sealed class UiString {
 
     data class FromString(val value: String) : UiString()
 
-    data class FromResources(@StringRes val id: Int) : UiString()
+    data class FromResources(@StringRes val id: Int, val lowercase: Boolean = false) : UiString()
 
-    class FromResourcesWithArgs(@StringRes val id: Int, vararg val args: Any) : UiString()
+    class FromResourcesWithArgs(@StringRes val id: Int, vararg val args: Any, val lowercase: Boolean = false) : UiString()
 
-    data class FromResourcesWithUiString(@StringRes val id: Int, val str: UiString, val separator: UiString) : UiString()
+    data class FromResourcesWithUiString(@StringRes val id: Int, val str: UiString, val separator: UiString, val lowercase: Boolean = false) : UiString()
+
+    class FromUiStrings(val strs: Array<UiString>, val separator: UiString) : UiString()
 
     @Composable
     fun asCompose(): String = when (this) {
         is FromString -> value
-        is FromResources -> stringResource(id)
-        is FromResourcesWithArgs -> stringResource(id, *args)
+        is FromResources -> {
+            if (lowercase) {
+                stringResource(id).lowercase()
+            } else {
+                stringResource(id)
+            }
+        }
+        is FromResourcesWithArgs -> {
+            if (lowercase) {
+                stringResource(id, *args).lowercase()
+            } else {
+                stringResource(id, *args)
+            }
+        }
         is FromResourcesWithUiString -> {
-            StringBuilder().apply {
+            val text = StringBuilder().apply {
                 append(stringResource(id))
                 append(separator.asCompose())
                 append(str.asCompose())
+            }.toString()
+
+            if (lowercase) {
+                text.lowercase()
+            } else {
+                text
+            }
+        }
+        is FromUiStrings -> {
+            StringBuilder().apply {
+                strs.forEachIndexed { index, str ->
+                    append(str.asCompose())
+                    if (index < strs.lastIndex) {
+                        append(separator.asCompose())
+                    }
+                }
             }.toString()
         }
     }
