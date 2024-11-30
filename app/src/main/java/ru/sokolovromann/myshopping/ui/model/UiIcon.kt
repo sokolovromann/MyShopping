b03241d835dcae1fd7968fea83044af1e62960c9
Roman Sokolov <sokolovromann@gmail.com>
2024-11-30 08:25:18 +0300

    Add Default Icons

--- app/src/main/java/ru/sokolovromann/myshopping/ui/model/UiIcon.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/model/UiIcon.kt
@@ -1,17 +1,34 @@
 package ru.sokolovromann.myshopping.ui.model
 
 import androidx.annotation.DrawableRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
+import ru.sokolovromann.myshopping.R
 
 sealed class UiIcon {
 
     data class FromVector(val imageVector: ImageVector) : UiIcon()
 
     data class FromResources(@DrawableRes val id: Int) : UiIcon()
+
+    companion object {
+        val Back: UiIcon = FromVector(Icons.Default.ArrowBack)
+        val Cancel: UiIcon = FromVector(Icons.Default.Clear)
+        val Clear: UiIcon = FromVector(Icons.Default.Clear)
+        val Close: UiIcon = FromVector(Icons.Default.Close)
+        val Delete: UiIcon = FromVector(Icons.Default.Delete)
+        val NavigationMenu: UiIcon = FromVector(Icons.Default.Menu)
+        val SelectAll: UiIcon = FromResources(R.drawable.ic_all_select_all)
+    }
 
     @Composable
     fun asPainter(): Painter = when (this) {
