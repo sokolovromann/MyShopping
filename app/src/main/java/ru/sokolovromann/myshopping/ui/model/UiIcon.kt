79d96ead4e2cea256758a4efc048467bfc43a54a
Roman Sokolov <sokolovromann@gmail.com>
2024-12-01 14:08:14 +0300

    Add defaults icons

--- app/src/main/java/ru/sokolovromann/myshopping/ui/model/UiIcon.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/ui/model/UiIcon.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
@@ -21,13 +22,18 @@ sealed class UiIcon {
     data class FromResources(@DrawableRes val id: Int) : UiIcon()
 
     companion object {
+        val Archive: UiIcon = FromResources(R.drawable.ic_all_archive)
         val Back: UiIcon = FromVector(Icons.Default.ArrowBack)
         val Cancel: UiIcon = FromVector(Icons.Default.Clear)
         val Clear: UiIcon = FromVector(Icons.Default.Clear)
         val Close: UiIcon = FromVector(Icons.Default.Close)
         val Delete: UiIcon = FromVector(Icons.Default.Delete)
+        val DisplayHidden: UiIcon = FromVector(Icons.Default.KeyboardArrowDown)
         val NavigationMenu: UiIcon = FromVector(Icons.Default.Menu)
+        val Reminder: UiIcon = FromResources(R.drawable.ic_all_reminder)
+        val Restore: UiIcon = FromResources(R.drawable.ic_all_restore)
         val SelectAll: UiIcon = FromResources(R.drawable.ic_all_select_all)
+        val Unarchive: UiIcon = FromResources(R.drawable.ic_all_unarchive)
     }
 
     @Composable
