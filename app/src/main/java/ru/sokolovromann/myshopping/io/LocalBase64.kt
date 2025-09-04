00a9ef0b283268145ca1710e1bb608399edad6c9
Roman Sokolov <sokolovromann@gmail.com>
2025-09-04 13:39:04 +0300

    Add LocalBase64

---
+++ app/src/main/java/ru/sokolovromann/myshopping/io/LocalBase64.kt
@@ -0,0 +1,19 @@
+package ru.sokolovromann.myshopping.io
+
+import ru.sokolovromann.myshopping.utils.Dispatcher
+import ru.sokolovromann.myshopping.utils.DispatcherExtensions.withContext
+import javax.inject.Inject
+import kotlin.io.encoding.Base64
+
+class LocalBase64 @Inject constructor() {
+
+    private val dispatcher = Dispatcher.IO
+
+    suspend fun encode(value: String): String = withContext(dispatcher) {
+        return@withContext Base64.encode(value.toByteArray())
+    }
+
+    suspend fun decode(value: String): String = withContext(dispatcher) {
+        return@withContext Base64.decode(value).decodeToString()
+    }
+}
