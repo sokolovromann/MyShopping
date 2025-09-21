549b3ba3a1758a5a6b93104ad14243544ee18f29
Roman Sokolov <sokolovromann@gmail.com>
2025-09-21 19:37:56 +0300

    Refactoring About

--- app/src/main/java/ru/sokolovromann/myshopping/about/GetAboutUseCase.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/about/GetAboutUseCase.kt
@@ -1,18 +1,25 @@
 package ru.sokolovromann.myshopping.about
 
+import ru.sokolovromann.myshopping.BuildConfig
 import ru.sokolovromann.myshopping.utils.Dispatcher
 import ru.sokolovromann.myshopping.utils.DispatcherExtensions.withContext
 import javax.inject.Inject
 
-class GetAboutUseCase @Inject constructor(
-    private val aboutDao: AboutDao,
-    private val aboutMapper: AboutMapper
-) {
+class GetAboutUseCase @Inject constructor() {
 
     private val dispatcher: Dispatcher = Dispatcher.IO
 
     suspend operator fun invoke(): About = withContext(dispatcher) {
-        val aboutEntity = aboutDao.getAbout()
-        return@withContext aboutMapper.mapTo(aboutEntity)
+        return@withContext About(
+            id = BuildConfig.APPLICATION_ID,
+            api = BuildConfig.VERSION_CODE.toLong(),
+            version = BuildConfig.VERSION_NAME,
+            developer = "Roman Sokolov",
+            email = "developer.myshopping@gmail.com",
+            linkedin = "",
+            github = "https://github.com/sokolovromann/MyShopping",
+            privacyPolicy = "https://sokolovromann.github.io/MyShopping/privacy_policy.html",
+            termsOfConditions = "https://sokolovromann.github.io/MyShopping/terms_and_conditions.html"
+        )
     }
 }
