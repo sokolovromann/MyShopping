4832180a4ad85740b82b42078a10ecce90581ced
Roman Sokolov <sokolovromann@gmail.com>
2025-10-09 13:57:01 +0300

    Refactoring DispatcherExtensions

--- app/src/main/java/ru/sokolovromann/myshopping/utils/DispatcherExtensions.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/utils/DispatcherExtensions.kt
@@ -11,6 +11,8 @@ import kotlinx.coroutines.withContext
 
 object DispatcherExtensions {
 
+    // WITH CONTEXT
+
     suspend fun <T> withContext(
         dispatcher: Dispatcher,
         block: suspend CoroutineScope.() -> T
@@ -20,6 +22,16 @@ object DispatcherExtensions {
             block = block
         )
     }
+
+    suspend fun <T> withIoContext(block: suspend CoroutineScope.() -> T): T {
+        return withContext(Dispatcher.IO, block)
+    }
+
+    suspend fun <T> withMainContext(block: suspend CoroutineScope.() -> T): T {
+        return withContext(Dispatcher.Main, block)
+    }
+
+    // ASYNC
 
     fun <T> CoroutineScope.async(
         dispatcher: Dispatcher,
@@ -31,6 +43,16 @@ object DispatcherExtensions {
         )
     }
 
+    fun <T> CoroutineScope.asyncOnIo(block: suspend CoroutineScope.() -> T): Deferred<T> {
+        return async(Dispatcher.IO, block)
+    }
+
+    fun <T> CoroutineScope.asyncOnMain(block: suspend CoroutineScope.() -> T): Deferred<T> {
+        return async(Dispatcher.Main, block)
+    }
+
+    // LAUNCH
+
     fun CoroutineScope.launch(
         dispatcher: Dispatcher,
         block: suspend CoroutineScope.() -> Unit
@@ -41,7 +63,25 @@ object DispatcherExtensions {
         )
     }
 
+    fun CoroutineScope.launchOnIo(block: suspend CoroutineScope.() -> Unit): Job {
+        return launch(Dispatcher.IO, block)
+    }
+
+    fun CoroutineScope.launchOnMain(block: suspend CoroutineScope.() -> Unit): Job {
+        return launch(Dispatcher.Main, block)
+    }
+
+    // FLOW ON
+
     fun <T> Flow<T>.flowOn(dispatcher: Dispatcher): Flow<T> {
         return flowOn(dispatcher.toCoroutineContext())
+    }
+
+    fun <T> Flow<T>.flowOnIo(): Flow<T> {
+        return flowOn(Dispatcher.IO)
+    }
+
+    fun <T> Flow<T>.flowOnMain(): Flow<T> {
+        return flowOn(Dispatcher.Main)
     }
 }
