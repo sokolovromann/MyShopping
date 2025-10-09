84a72f57c01c4a82d39472173ead195c5f146271
Roman Sokolov <sokolovromann@gmail.com>
2025-10-09 13:53:04 +0300

    Replace BackupConfig

---
+++ app/src/main/java/ru/sokolovromann/myshopping/data39/settings/backup/BackupConfig.kt
@@ -0,0 +1,5 @@
+package ru.sokolovromann.myshopping.data39.settings.backup
+
+data class BackupConfig(
+    val location: String
+)
--- app/src/main/java/ru/sokolovromann/myshopping/settings/backup/BackupConfig.kt
+++
@@ -1,5 +0,0 @@
-package ru.sokolovromann.myshopping.settings.backup
-
-data class BackupConfig(
-    val location: String
-)
