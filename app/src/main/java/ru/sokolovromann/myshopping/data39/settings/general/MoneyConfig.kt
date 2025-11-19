ffa2ca9b6e8a16674cd72638efdfcc2e3e48acd3
Roman Sokolov <sokolovromann@gmail.com>
2025-11-19 04:56:50 +0300

    Delete Data39 GeneralSettings

--- app/src/main/java/ru/sokolovromann/myshopping/data39/settings/general/MoneyConfig.kt
+++
@@ -1,9 +0,0 @@
-package ru.sokolovromann.myshopping.data39.settings.general
-
-import ru.sokolovromann.myshopping.utils.Decimal
-import ru.sokolovromann.myshopping.utils.DecimalConfig
-
-data class MoneyConfig(
-    val decimalConfig: DecimalConfig.Money,
-    val taxRate: Decimal
-)
