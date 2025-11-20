82e9fa54492fd8e3ae5bcce1b8915f27b33f7ef3
Roman Sokolov <sokolovromann@gmail.com>
2025-11-20 07:14:04 +0300

    Delete Data39 UserSettings

--- app/src/main/java/ru/sokolovromann/myshopping/data39/settings/user/UserConfig.kt
+++
@@ -1,5 +0,0 @@
-package ru.sokolovromann.myshopping.data39.settings.user
-
-data class UserConfig(
-    val api: String?
-)
--- app/src/main/java/ru/sokolovromann/myshopping/data39/settings/user/UserConfigScheme.kt
+++
@@ -1,9 +0,0 @@
-package ru.sokolovromann.myshopping.data39.settings.user
-
-import androidx.datastore.preferences.core.stringPreferencesKey
-
-object UserConfigScheme {
-    const val DATA_STORE_NAME: String = "local_user_config"
-
-    val API = stringPreferencesKey("api")
-}
