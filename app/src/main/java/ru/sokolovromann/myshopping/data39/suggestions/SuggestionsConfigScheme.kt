3838f5babd48bf6d73f401e34bed3db820b37f98
Roman Sokolov <sokolovromann@gmail.com>
2026-01-03 08:46:44 +0300

    Add SuggestionsConfig DataStore Scheme

---
+++ app/src/main/java/ru/sokolovromann/myshopping/data39/suggestions/SuggestionsConfigScheme.kt
@@ -0,0 +1,15 @@
+package ru.sokolovromann.myshopping.data39.suggestions
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object SuggestionsConfigScheme {
+    const val DATA_STORE_NAME: String = "local_suggestions_config"
+
+    val VIEW_MODE = stringPreferencesKey("view_mode")
+    val SORT = stringPreferencesKey("sort")
+    val SORT_ORDER = stringPreferencesKey("sort_order")
+    val TAKE_NAMES = stringPreferencesKey("take_names")
+    val TAKE_DESCRIPTIONS = stringPreferencesKey("take_descriptions")
+    val TAKE_QUANTITIES = stringPreferencesKey("take_quantities")
+    val TAKE_MONEY = stringPreferencesKey("take_money")
+}
