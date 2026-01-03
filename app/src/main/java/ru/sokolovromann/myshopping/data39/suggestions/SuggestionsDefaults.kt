5d43f2daebbe61e0badf9dbb127cdde4fb26fb02
Roman Sokolov <sokolovromann@gmail.com>
2026-01-03 08:46:24 +0300

    Add SuggestionsDefaults

---
+++ app/src/main/java/ru/sokolovromann/myshopping/data39/suggestions/SuggestionsDefaults.kt
@@ -0,0 +1,14 @@
+package ru.sokolovromann.myshopping.data39.suggestions
+
+import ru.sokolovromann.myshopping.utils.math.DiscountType
+
+object SuggestionsDefaults {
+    val DISCOUNT_TYPE: DiscountType = DiscountType.Money
+    val VIEW_MODE: SuggestionsViewMode = SuggestionsViewMode.List
+    val SORT_ORDER: SortSuggestions.Order = SortSuggestions.Order.ByAscending
+    val SORT: SortSuggestions = SortSuggestions.Name(SORT_ORDER)
+    val TAKE_SUGGESTIONS: TakeSuggestions = TakeSuggestions.Ten
+    val TAKE_DESCRIPTIONS: TakeSuggestionDetails = TakeSuggestionDetails.Three
+    val TAKE_QUANTITIES: TakeSuggestionDetails = TakeSuggestionDetails.Five
+    val TAKE_MONEY: TakeSuggestionDetails = TakeSuggestionDetails.Three
+}
