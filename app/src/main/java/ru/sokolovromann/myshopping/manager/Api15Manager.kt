cf8cd96a4cd1da755bb1e74d27baae299cc4e18c
Roman Sokolov <sokolovromann@gmail.com>
2026-02-13 08:12:00 +0300

    Add CountDetails

--- app/src/main/java/ru/sokolovromann/myshopping/manager/Api15Manager.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/manager/Api15Manager.kt
@@ -5,6 +5,7 @@ import ru.sokolovromann.myshopping.data39.old.Api15AutocompletesConfigEntity
 import ru.sokolovromann.myshopping.data39.old.Api15Repository
 import ru.sokolovromann.myshopping.utils.DispatcherExtensions.withIoContext
 import javax.inject.Inject
+import kotlin.collections.distinctBy
 
 class Api15Manager @Inject constructor(private val api15Repository: Api15Repository) {
 
@@ -15,65 +16,79 @@ class Api15Manager @Inject constructor(private val api15Repository: Api15Reposit
             .mapKeys { (key, _) -> key.replaceFirstChar { it.uppercase() } }
             .mapValues { (_, value) ->
                 val sorted = value.sortedBy { it.lastModified }
-                val details = mutableListOf<Api15AutocompleteEntity>()
-
-                val manufacturer = sorted
-                    .distinctBy { it.manufacturer.lowercase() }
-                    .filter { it.manufacturer.isNotEmpty() }
-                details.addAll(manufacturer)
-
-                val brands = sorted
-                    .distinctBy { it.brand.lowercase() }
-                    .filter { it.brand.isNotEmpty() }
-                details.addAll(brands)
-
-                val sizes = sorted
-                    .distinctBy { it.size.lowercase() }
-                    .filter { it.size.isNotEmpty() }
-                details.addAll(sizes)
-
-                val colors = sorted
-                    .distinctBy { it.color.lowercase() }
-                    .filter { it.color.isNotEmpty() }
-                details.addAll(colors)
-
-                val quantities = sorted
-                    .distinctBy { "${it.quantity} ${it.quantitySymbol.lowercase()}" }
-                    .filter { it.quantity > 0f }
-                details.addAll(quantities)
-
-                val unitPrices = sorted
-                    .distinctBy { it.price }
-                    .filter { it.price > 0f }
-                details.addAll(unitPrices)
-
-                val discounts = sorted
-                    .distinctBy { "${it.discount} ${it.discountAsPercent}" }
-                    .filter { it.discount > 0f }
-                details.addAll(discounts)
-
-                val taxRates = sorted
-                    .distinctBy { it.taxRate }
-                    .filter { it.taxRate > 0f }
-                details.addAll(taxRates)
-
-                val costs = sorted
-                    .distinctBy { it.total }
-                    .filter { it.total > 0f }
-                details.addAll(costs)
-
-                details.toList()
+                mutableListOf<Api15AutocompleteEntity>().apply {
+                    addAll(sorted.filter("Manufacturer"))
+                    addAll(sorted.filter("Brands"))
+                    addAll(sorted.filter("Sizes"))
+                    addAll(sorted.filter("Colors"))
+                    addAll(sorted.filter("Quantities"))
+                    addAll(sorted.filter("UnitPrices"))
+                    addAll(sorted.filter("Discounts"))
+                    addAll(sorted.filter("TaxRates"))
+                    addAll(sorted.filter("Costs"))
+                }
             }
-    }
-
-    suspend fun countAutocompleteNames(name: String): Int = withIoContext {
-        val displayDefault = getAutocompletesConfig().displayDefaultAutocompletes
-        return@withIoContext api15Repository.getAutocompletes(displayDefault).count {
-            it.name.equals(name, true)
-        }
     }
 
     suspend fun getAutocompletesConfig(): Api15AutocompletesConfigEntity = withIoContext {
         return@withIoContext api15Repository.getAutocompletesConfig()
+    }
+
+    suspend fun countAutocompleteNames(name: String): Int = withIoContext {
+        val displayDefault = getAutocompletesConfig().displayDefaultAutocompletes
+        return@withIoContext api15Repository.getAutocompletes(displayDefault)
+            .count { it.name.equals(name, true) }
+    }
+
+    suspend fun countAutocompleteDetails(name: String, detailName: String): Int = withIoContext {
+        val displayDefault = getAutocompletesConfig().displayDefaultAutocompletes
+        return@withIoContext api15Repository.getAutocompletes(displayDefault)
+            .filter { it.name.equals(name, true) }
+            .filter(detailName)
+            .count()
+    }
+
+    private fun Collection<Api15AutocompleteEntity>.filter(field: String): List<Api15AutocompleteEntity> {
+        return when (field) {
+            "Manufacturer" -> {
+                distinctBy { it.manufacturer.lowercase() }
+                    .filter { it.manufacturer.isNotEmpty() }
+            }
+            "Brands" -> {
+                distinctBy { it.brand.lowercase() }
+                    .filter { it.brand.isNotEmpty() }
+            }
+            "Sizes" -> {
+                distinctBy { it.size.lowercase() }
+                    .filter { it.size.isNotEmpty() }
+            }
+            "Colors" -> {
+                distinctBy { it.color.lowercase() }
+                    .filter { it.color.isNotEmpty() }
+            }
+            "Quantities" -> {
+                distinctBy { "${it.quantity} ${it.quantitySymbol.lowercase()}" }
+                    .filter { it.quantity > 0f }
+            }
+            "UnitPrices" -> {
+                distinctBy { it.price }
+                    .filter { it.price > 0f }
+            }
+            "Discounts" -> {
+                distinctBy { "${it.discount} ${it.discountAsPercent}" }
+                    .filter { it.discount > 0f }
+            }
+            "TaxRates" -> {
+                distinctBy { it.taxRate }
+                    .filter { it.taxRate > 0f }
+            }
+            "Costs" -> {
+                distinctBy { it.total }
+                    .filter { it.total > 0f }
+            }
+            else -> {
+                this.toList()
+            }
+        }
     }
 }
