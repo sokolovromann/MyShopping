7f87a7aae98230dc4c506b2dc1be573e4888daf6
Roman Sokolov <sokolovromann@gmail.com>
2026-02-18 08:54:53 +0300

    Add create uid from string

--- app/src/main/java/ru/sokolovromann/myshopping/utils/UID.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/utils/UID.kt
@@ -1,5 +1,6 @@
 package ru.sokolovromann.myshopping.utils
 
+import java.nio.charset.StandardCharsets
 import java.util.UUID
 
 @JvmInline
@@ -8,6 +9,13 @@ value class UID(val value: String) {
     companion object {
         fun createRandom(): UID {
             val value = UUID.randomUUID().toString()
+            return UID(value)
+        }
+        fun createFromString(str: String): UID {
+            val formatted = str.trim().lowercase()
+            val value = UUID.nameUUIDFromBytes(
+                formatted.toByteArray(StandardCharsets.UTF_8)
+            ).toString()
             return UID(value)
         }
     }
