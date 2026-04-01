bb9431e8af25b04f667f504f3beb6eea6237b030
Roman Sokolov <sokolovromann@gmail.com>
2026-04-01 08:06:18 +0300

    Refactor StringUtils

--- app/src/main/java/ru/sokolovromann/myshopping/data/utils/StringUtils.kt
+++ app/src/main/java/ru/sokolovromann/myshopping/data/utils/StringUtils.kt
@@ -1,16 +1,5 @@
 package ru.sokolovromann.myshopping.data.utils
 
-fun String.uppercaseFirst(): String {
-    return if (this.isEmpty()) {
-        this
-    } else {
-        this.replaceFirst(
-            oldValue = this.first().toString(),
-            newValue = this.first().uppercase()
-        )
-    }
-}
-
 fun String.asSearchQuery(): String {
     return this.lowercase()
 }
