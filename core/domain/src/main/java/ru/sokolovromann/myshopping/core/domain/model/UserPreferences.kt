b463851b562473fa4a7d0e65d08435a7a5251fc5
Roman Sokolov <sokolovromann@gmail.com>
2026-06-02 07:19:24 +0300

    Add :core:domain UserPreferences

---
+++ core/domain/src/main/java/ru/sokolovromann/myshopping/core/domain/model/UserPreferences.kt
@@ -0,0 +1,10 @@
+package ru.sokolovromann.myshopping.core.domain.model
+
+data class UserPreferences(
+    val general: GeneralPreferences,
+    val carts: CartsPreferences,
+    val products: ProductsPreferences,
+    val productsWidget: ProductsWidgetPreferences,
+    val addEditProduct: AddEditProductPreferences,
+    val suggestions: SuggestionsPreferences
+)
