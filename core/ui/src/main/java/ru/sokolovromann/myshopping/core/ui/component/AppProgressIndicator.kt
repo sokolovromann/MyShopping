bbe8e89cfb7744beff57380e7a6e3e991ac422fd
Roman Sokolov <sokolovromann@gmail.com>
2026-08-12 08:22:39 +0300

    Rename to AppProgressIndicator

--- core/ui/src/main/java/ru/sokolovromann/myshopping/core/ui/component/AppProgressIndicator.kt
+++ core/ui/src/main/java/ru/sokolovromann/myshopping/core/ui/component/AppProgressIndicator.kt
@@ -1,45 +1,34 @@
-package ru.sokolovromann.myshopping.core.component
+package ru.sokolovromann.myshopping.core.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ru.sokolovromann.myshopping.core.ui.theme.MyShoppingTheme
 
 @Composable
-fun DefaultProgressIndicator(
-    modifier: Modifier = Modifier,
-    text: String? = null
-) {
+fun AppProgressIndicator(text: String? = null) {
     Column(
-        modifier = modifier
+        modifier = Modifier
             .background(Color.Transparent)
-            .padding(8.dp),
+            .padding(all = 8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CircularProgressIndicator()
         text?.let {
             Text(
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier.padding(vertical = 16.dp),
+                style = MaterialTheme.typography.titleMedium,
                 text = it
             )
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun DefaultProgressIndicatorPreview() {
-    MyShoppingTheme {
-        DefaultProgressIndicator(text = "Please, wait...")
     }
 }
