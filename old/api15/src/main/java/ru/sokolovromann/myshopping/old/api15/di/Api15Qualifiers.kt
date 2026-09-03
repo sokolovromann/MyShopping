9330d43fcd6a1b8aad39d8e085d694ae595de150
Roman Sokolov <sokolovromann@gmail.com>
2026-09-03 11:37:58 +0300

    Delete migration modules

--- old/api15/src/main/java/ru/sokolovromann/myshopping/old/api15/di/Api15Qualifiers.kt
+++
@@ -1,8 +0,0 @@
-package ru.sokolovromann.myshopping.old.api15.di
-
-import jakarta.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION)
-annotation class Api15DataStore
--- old/api15/src/main/java/ru/sokolovromann/myshopping/old/api15/model/AppBuildConfig.kt
+++
@@ -1,6 +0,0 @@
-package ru.sokolovromann.myshopping.old.api15.model
-
-data class AppBuildConfig(
-    val appFirstTime: String? = null,
-    val userCodeVersion: Int? = null
-)
