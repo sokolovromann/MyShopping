17962180f34e8b5d3895c6de1af15cf59320852c
Roman Sokolov <sokolovromann@gmail.com>
2026-09-15 07:51:54 +0300

    Refactor TimeInMillis

--- core/domain/src/main/java/ru/sokolovromann/myshopping/core/domain/model/TimeInMillis.kt
+++ core/domain/src/main/java/ru/sokolovromann/myshopping/core/domain/model/TimeInMillis.kt
@@ -3,15 +3,7 @@ package ru.sokolovromann.myshopping.core.domain.model
 @JvmInline
 value class TimeInMillis(val value: Long) {
 
-    init {
-        require(value >= MIN.value && value <= MAX.value) {
-            "The value must be greater than or equal ${MIN.value} and less than ${MAX.value}."
-        }
-    }
-
     companion object {
-        val MIN: TimeInMillis = TimeInMillis(1L)
-        val MAX: TimeInMillis = TimeInMillis(Long.MAX_VALUE)
         fun getCurrent(): TimeInMillis = TimeInMillis(System.currentTimeMillis())
     }
 
